f6b7aa2b5dfbcc15a79ac002733f63252d6e4bb4
robwilliams-it <robwilliams.it@gmail.com>
2021-12-01 12:55:41 -0600

    added all of the raw data for work history

---
+++ src/javascript/work_history/fendi.js
@@ -0,0 +1,20 @@
+export default Fendi = {
+    positions: [
+        {
+            title: "Help Desk Support (Contract)",
+            details: [
+                "Provided on-site, and remote support to all corporate users as well as stores by determining source of computer malfunction by using diagnostics and application software to adjust, repair, and replace malfunctioning equipment on a need basis",
+                "Provided support to stores and ensured computer, printer, and terminal, and POS application functionality", 
+                "Managed projects to upgrade users with various tools in order to improve self sufficiency",
+                "Managed Avaya phone system using IP Management application to reset passwords and modify accounts",
+                "Organized and managed schedule for deployments, deployed workstations for new hires, reset and linked Blackberry Handheld accounts as needed", 
+                "Responded to end-users and provide technical expertise with regard to hardware and software upgrades as needed",
+                "Managed IT for Sample Sale which required workstation configuration for system and POS communication",
+            ],
+            start: 0,
+            end: 1,
+        }
+    ]
+}
+
+
---
+++ src/javascript/work_history/moda.js
@@ -0,0 +1,43 @@
+export default Fendi = {
+    positions: [
+        {
+            title: "IT Manager",
+            details: [
+                "Lead team of 3 with supporting over 400 employee company",
+                "Created implementation plans, accounting for ROI, and other cost-benefit analyses",
+                "Collaborated with cross functional teams to identify opportunities to deliver added value",
+            ],
+            start: 0,
+            end: 1,
+        },
+
+        {
+            title: "Senior Desktop Support Engineer",
+            details: [
+                "Implement user focused ticketing system and reporting tools within Jira",
+                "Supervised team of 2 to support over 300 employees",
+                "Manage the IT aspect of new office buildout incorporating network, AV, and IT infrastructure",
+                "Closely collaborate with project members to identify and quickly address problems and  adjust plans to account for dynamic targets",
+            ],
+            start: 0,
+            end: 1,
+        },
+
+        {
+            title: "Helpdesk Support Technician",
+            details: [
+                "Provide IT support for a company scaling from 60 - 200 users in a fast paced environment",
+                "Improved and standardized the AV setup for each conference space based on room layout and occupancy size",
+                "Identify and resolve process bottlenecks to encourage smoother procedures, and more efficient workflows",
+                "Automat processes using various scripting languages to reduce task time",
+                "Organize and maintain IT best practice documentation for new and existing staff",
+                "Centrally manage all company accounts for tech related spending",
+                "Manage all company assets within asset management system",
+            ],
+            start: 0,
+            end: 1,
+        }
+    ]
+}
+
+
