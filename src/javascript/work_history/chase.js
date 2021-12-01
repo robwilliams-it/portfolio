02db6cea3d102671ce90109cad770161fe0d6330
robwilliams-it <robwilliams.it@gmail.com>
2021-12-01 12:26:39 -0600

    initialized project and started to compile data from resume

---
+++ src/javascript/work_history/chase.js
@@ -0,0 +1,12 @@
+export default Chase = {
+    title: "Investment Banking Technology Operations Analyst",
+    details: [
+        "Provided technical support to clients in five different buildings throughout Manhattan, including high level executives, support staff and consultants",
+        "Installed operating systems and applications by use of images, mapping network drives and printers, deploying and setting up workstations, desktops as well as virtual desktops, laptops, and configure PCs to maximize proficiency and enable user preferences",
+        "Kept track of inventory of more than 1,000 PC’s as well as monitors and other technical peripherals for re-deployment and disposal purposes",
+        "Worked with the Analyst and Associates Group to setup laptops with appropriate applications, wireless networks, security, and user profiles for 220 analysts",
+        "Handled trouble tickets and maintained workstation performance for 300 analysts in two different groups simultaneously",
+    ],
+    start: 0,
+    end: 1,
+}
